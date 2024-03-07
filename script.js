ee62262f7a3a87e350a3b341da54b3de7cfc7fa4
PriyeshGawai <120727228+PriyeshGawai@users.noreply.github.com>
2024-03-07 09:47:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Welcome to spotify");
 
-let songIndex=0;
+let songIndex=1;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('MyProgressBar');
